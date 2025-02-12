68c86213e5a09ea217a30653693aa3d9ca8fa8db
YsacAdvincula <152839626+YsacAdvincula@users.noreply.github.com>
2025-02-12 05:52:55 +0000

    State_Diagram

---
+++ Account.java
@@ -0,0 +1,57 @@
+class Account {
+    private String accountNumber;
+    private double balance;
+    private AccountState accountState;
+
+    public Account(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+        this.accountState = new ActiveState();
+        System.out.println("New Account Created. " + this);
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setAccountState(AccountState accountState) {
+        this.accountState = accountState;
+    }
+
+    public AccountState getAccountState() {
+        return accountState;
+    }
+
+    public void deposit(double amount) {
+        accountState.deposit(this, amount);
+    }
+
+    public void withdraw(double amount) {
+        accountState.withdraw(this, amount);
+    }
+
+    public void suspend() {
+        accountState.suspend(this);
+    }
+
+    public void activate() {
+        accountState.activate(this);
+    }
+
+    public void close() {
+        accountState.close(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Account Number:" + accountNumber + ", Balance:" + balance + ", State:" + accountState.getStateName();
+    }
+}
---
+++ ActiveState.java
@@ -0,0 +1,36 @@
+class ActiveState implements AccountState {
+    
+    @Override
+    public void deposit(Account account, double amount) {
+        account.setBalance(account.getBalance() + amount);
+        System.out.println("Deposit successful. " + account);
+    }
+
+    @Override
+    public void withdraw(Account account, double amount) {
+        account.setBalance(account.getBalance() - amount);
+        System.out.println("Withdrawal successful. " + account);
+    }
+
+    @Override
+    public void suspend(Account account) {
+        account.setAccountState(new SuspendedState());
+        System.out.println("Account is suspended!");
+    }
+
+    @Override
+    public void activate(Account account) {
+        System.out.println("Account is already activated!");
+    }
+
+    @Override
+    public void close(Account account) {
+        account.setAccountState(new ClosedState());
+        System.out.println("Account is closed!");
+    }
+
+    @Override
+    public String getStateName() {
+        return "Active";
+    }
+}
